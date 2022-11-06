dab301c8f09195db37994f335e6b3c8c2ea10b00
Tapia-Jonathan <jonathan.ezequiel.tapia@gmail.com>
2022-11-06 15:28:37 -0300

    re-merge delgando

--- TP/tp_refactorizado/clasePrenda.py
+++ TP/tp_refactorizado/clasePrenda.py
@@ -6,6 +6,10 @@ class Prenda:
         self.precio=precio
         self.categoria=categoria
         self.stock=0
+        self.estado=Nueva()
+
+    def restaurar_stock(self):
+        self.stock=0
     def codigo_(self):
         return int(self.codigo)
     def nombre_(self):
@@ -23,8 +27,44 @@ class Prenda:
         self.stock-=stock
     def hay_stock(self):
         return not self.stock==0
-
     
+    def es_de_categoria(self,cate):
+        return self.categoria==cate
+    
+    def es_de_nombre(self,name):
+        return self.nombre==name
+
+    def actualizar_precio_segun_porcentaje(self,porcentaje):
+        self.precio+=self.precio * porcentaje / 100
+
+    def cambiar_estado(self,estado_nuevo):
+        self.estado = estado_nuevo
+        self.precio=self.estado.precio(self.precio)
+
+    def calcular_precio_final(self, es_extranjero):
+        if self.precio > 70 and es_extranjero:
+            return self.precio
+        else:
+            costo_final = self.precio* 1.21
+            return costo_final
+
+class Nueva:
+    def precio(self,precio):
+        return precio
+class Promocion:
+    def __init__(self, promo):
+        self.promo = promo
+    def precio(self,precio):
+        return precio - self.promo
+class Liquidacion:
+    def precio(self,precio):
+        return precio / 2
+
+
 remera_m=Prenda(100,"remera m","remera",4500)
 remera_s = Prenda(99,"remera de talle s", "remera", 4500)
 pantalon_m = Prenda(444,"pantalon talle m", "pantalon", 6000)
+
+promo_300=Promocion(300)
+liquidacion=Liquidacion()
+nueva=Nueva()
--- TP/tp_refactorizado/tp_refactorizado.py
+++ TP/tp_refactorizado/tp_refactorizado.py
@@ -1,12 +1,15 @@
+import csv
 from clasePrenda import *
 from criterios import *
 from datetime import date
 hoy = date.strftime(date.today(), "%Y-%m-%d")
+from operator import itemgetter
 class Local:
     def __init__(self):
         self.productos=[]
         self.ventas=[]
         self.codigos=[]
+        
 
 
     def reiniciar_listas(self):
@@ -21,6 +24,10 @@ class Local:
             else:
                 self.productos.append(objeto)
                 self.codigos.append(objeto.codigo_())
+    
+    def recargar_stock(self,objeto,cantidad):
+        
+        objeto.recargar_stock(cantidad)
 
     def contar_categorias(self):
         categorias = []
@@ -46,20 +53,76 @@ class Local:
                     self.ventas.append(venta)
 
     def discontinuar_productos(self):
-        dic_aux = self.productos[:]
-        for producto in dic_aux:
-            if producto.stock_() == 0:
+        # dic_aux = self.productos[:]
+        # for producto in dic_aux:
+        #     if not  producto.hay_stock():
+        #         self.productos.remove(producto)
+        #         self.codigos.remove(producto.codigo_())
+
+        for producto in list(self.productos):
+            if not producto.hay_stock():
                 self.productos.remove(producto)
                 self.codigos.remove(producto.codigo_())
 
-        for producto in self.productos:
-            if producto.stock_() == 0:
-                self.productos.remove(producto)
-                self.codigos.remove(producto.codigo_())
+
+    def ventas_del_dia(self):
+        ventas = 0
+        for venta in self.ventas:
+            if venta["fecha"] == hoy:
+                ventas += 1
+        return ventas
+    def valor_ventas_del_dia(self):
+        total_ventas_del_dia = 0
+        subtotal = 0
+        for venta_de_hoy in self.ventas:
+            if venta_de_hoy["fecha"] == hoy:
+                subtotal = venta_de_hoy["precio_total"] #* venta_de_hoy["cantidad"]
+                total_ventas_del_dia += subtotal
+        return total_ventas_del_dia
+
+    def cantidad_ventas_del_dia(self):
+        cantidad = 0
+        for venta_de_hoy in self.ventas:
+            if venta_de_hoy["fecha"] == hoy:
+                subtotal = venta_de_hoy["cantidad"]
+                cantidad += subtotal
+        return cantidad
+
+    def ventas_del_anio(self):
+        ventas_del_anio_actual = []
+        anio_actual = hoy[:4]
+        for producto in self.ventas:
+            if producto["fecha"][:4] == anio_actual:
+                ventas_del_anio_actual.append(producto)
+        return ventas_del_anio_actual
     
 
+    def productos_mas_vendidos(self,cantidad):
+        lista_mas_vendidos = []
+        nombres_mas_vendidos = []
+        for producto_venta in self.ventas:
+            for producto in self.productos:
+                if producto.codigo_() == producto_venta["codigo_producto"]:
+                    producto_y_cantidad = {
+                        "nombre": producto.nombre_(),
+                        "cantidad": producto_venta["cantidad"]
+                    }
+                    lista_mas_vendidos.append(producto_y_cantidad)
+        ventas_ordenadas_may_a_menor = sorted(lista_mas_vendidos, key=itemgetter("cantidad"), reverse=True)
+        for producto in ventas_ordenadas_may_a_menor:
+            if producto["nombre"] not in nombres_mas_vendidos:
+                nombres_mas_vendidos.append(producto["nombre"])
+        return nombres_mas_vendidos[:cantidad]
 
-
+    def actualizar_precio_segund(self,criterio,porcentaje):
+        for producto in self.productos:
+            if criterio.aplica_a(producto):
+                producto.actualizar_precio_segun_porcentaje(porcentaje)
+    def reporte_diario(self):
+        self.reporte.append([hoy, self.cantidad_ventas_del_dia(), self.valor_ventas_del_dia()])
+        with open("reporte.csv", "w", newline="") as file:
+            writer = csv.writer(file, delimiter=",")
+            writer.writerows(self.reporte)
 
 
 class Fisico(Local):
@@ -93,3 +156,27 @@ class Virtual(Local):
 
 
 local=Local()
+
+localvirtual = Virtual(1000)
+localfisico = Fisico(77500)
+remera_m = Prenda(100,"remera talle m", "remera", 4500)
+pulsera = Prenda(1098,"pulserita de tela verde", "accesorios", 50)
+remera_s = Prenda(99,"remera de talle s", "remera", 4500)
+campera_l = Prenda(555,"campera talle l", "campera", 35000)
+pantalon_m = Prenda(444,"pantalon talle m", "pantalon", 6000)
+promo_500 = Promocion(500)
+liquidacion = Liquidacion()
+nueva = Nueva()
+
+
+
+localvirtual = Virtual(1000)
+localfisico = Fisico(77500)
+remera_m = Prenda(100,"remera talle m", "remera", 4500)
+pulsera = Prenda(1098,"pulserita de tela verde", "accesorios", 50)
+remera_s = Prenda(99,"remera de talle s", "remera", 4500)
+campera_l = Prenda(555,"campera talle l", "campera", 35000)
+pantalon_m = Prenda(444,"pantalon talle m", "pantalon", 6000)
+promo_500 = Promocion(500)
+liquidacion = Liquidacion()
+nueva = Nueva()
