061bf5602e4434c3446f519a9ef1d08af558459d
Tapia-Jonathan <jonathan.ezequiel.tapia@gmail.com>
2022-11-08 21:57:36 -0300

    se implementa modulo persitencia

--- TP/eliminar_sin_stock.py
+++ TP/eliminar_sin_stock.py
@@ -1,4 +1,4 @@
-from tp_macowins import *
+from local import *
 from datetime import datetime
 from persistencia import *
 import subprocess
@@ -8,8 +8,13 @@ subprocess.call(["zenity", "--info",  '--title=Semana terminada', "--text=Se eli
 
 def main():
     print(datetime.now(), "Eliminando productos sin stock")
+    guardar("ficheroExterno",localfisico.productos)
+    print("Productos almacenados: " + str(len(localfisico.productos)))
+    print("Productos sin stock: " + str(localfisico.productos_sin_stock()))
     localfisico.discontinuar_productos()
-    localvirtual.discontinuar_productos()
+    cargar("ficheroExterno")
+    cargar_todos()
+    print("Productos disponibles: " +str(len(localfisico.productos)))
     print(datetime.now(), "Productos sin stock eliminados")
 
 if __name__ == "__main__":
