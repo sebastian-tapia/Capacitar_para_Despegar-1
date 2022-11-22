afd73e352c112574edc00aa552280f58afc7bd73
Tapia-Jonathan <jonathan.ezequiel.tapia@gmail.com>
2022-11-22 15:15:27 -0300

    maquetado con templates

--- Maquetado/17-11-22/app.py
+++ Maquetado/17-11-22/app.py
@@ -7,31 +7,12 @@ app = Flask(__name__)
 def raiz():
     return render_template("home.html")
 
-
-# @app.get ("/")   #la raiz (ruta)
-# def hola_mundo():
-#     return f"""
-#     <h1>hola {request.args.get("nombre","mundo")}!</h1>
-#     <p>Comando para cambiar el puerto: <br><br> <code>flask --debug run --port 8080</code></p>
-#     """
-
 @app.get("/saludo")
 def saludo():
-    return render_template("saludo.html")
+    nombre = request.form.get("nombre")
+    return render_template("saludo.html", nombre = nombre)
 
 
 @app.get("/recientes")
 def recientes():
     return render_template("recientes.html")
-    
-    
-    # f"""
-    # <h1>hola {request.args.get("nombre","mundo")}!</h1>
-    # <p>Comando para cambiar el puerto: <br><br> <code>flask --debug run --port 8080</code></p>
-    # """
-
-print("Iniciando servidor")
-
-
-#hacer lo mismo con despedida
-#hacer una funcion para request.arg para no repetir codigo
